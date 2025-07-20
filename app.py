0d472810b6b98be0f460fb3a21971531731be4ea
Jean-snt <1611542@senati.pe>
2025-07-19 19:24:49 -0500

    fix: corregir error en conversión de grados

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
+
 
 @app.route('/script.js')
 def script():
